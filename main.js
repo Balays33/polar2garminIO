e614e1e31ac87d2cf67e055d7d736b007681f98c
bbarczawork <bbarczawork@gmail.com>
2024-12-04 20:16:16 +0000

    new

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-// Add JS here
+
+function downloadFile() {
+    fetch('./GarminReady.txt')
+      .then(response => response.text())
+      .then(text => {
+        // Process the text content here (optional)
+        console.log(text);
+  
+        // Create a Blob object and download link
+        const blob = new Blob([text], { type: 'text/plain' }); // Adjust MIME type as needed
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        // Use a fixed download name or modify based on logic
+        a.download = 'GarminReady.txt';
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+        URL.revokeObjectURL(url);
+      })
+      .catch(error => {
+        console.error('Error fetching file:', error);
+      });
+  }
+ 
+
+
