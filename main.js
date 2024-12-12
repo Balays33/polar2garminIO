ea0e07372a0a2707a294e645a5e9e63a3d2829db
bbarczawork <bbarczawork@gmail.com>
2024-12-12 22:37:08 +0000

    next step the repalce

--- main.js
+++ main.js
@@ -24,40 +24,86 @@ function downloadFile() {
 }
 
 function upload() {
-  console.log("upload IS WORKING")
+  console.log("This is the upload function")
 
   const fileInput = document.getElementById('fileInput');
   const file = fileInput.files[0];
+
+  // Get file name
+  const fileName = file.name;
+
+  // Get file size
+  const fileSize = file.size;
+
+  // Get file type
+  const fileType = file.type;
+
+  // Get last modified date
+  const lastModified = file.lastModifiedDate;
+
+  // Now you can use these properties as needed
+  console.log('File Name:', fileName);
+  console.log('File Size:', fileSize);
+  console.log('File Type:', fileType);
+  console.log('Last Modified:', lastModified);
+
   const reader = new FileReader();
-
-  const elementHolder = document.getElementById('newtext');
-  elementHolder.textContent = "Balazs";
-
 
   reader.onload = function (e) {
     const fileContent = e.target.result;
-    // console.log(fileContent); // Print the file content to the console
+    console.log('File Content:', fileContent);
 
-    
+    fixthefile(fileContent);
 
-    //input = new FileReader('./GarminReady.txt')
-    //console.log(input);
+    writefile(fileContent);
 
-
+    const elementHolder2 = document.getElementById('newtext');
+    elementHolder2.textContent = fileName;
 
   };
 
-  //reader.readAsText(file); // Read the file as text
-  writefile(file)
+  reader.readAsText(file); // Read the file as text
+
+}
+
+function fixthefile(fileContent, str, start, end) {
+  console.log("This is the fix function")
+
+  
+    const result = str.match(new RegExp(start + "(.*)" + end));
+
+    return result[1];
 
 
+const testString = '124 photos in 22 photo albums';
+
+console.log(getStringBetween(testString, 'in ', ' photo'));
+
+
+
+  return fileContent;
 }
 
 function writefile(fileContent) {
-  console.log("writefile IS WORKING")
-  console.log(fileContent)
+  console.log("writefile IS WORKING");
 
+  // Create a Blob object with the file content
+
+  const blob = new Blob([fileContent], { type: 'text/plain' });
+
+  // Create a download link
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = 'GarminReady.TCX'; 
+
+  // Trigger the download
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+  URL.revokeObjectURL(url); 
 }
+
 
 function testmain() {
   console.log("test it has to sort out")
@@ -66,6 +112,6 @@ function testmain() {
 
   const elementHolder2 = document.getElementById('newtext');
   elementHolder2.textContent = 'Balazs';
-  
+
 }
 
